d1d56e5ed9f63f8a4f446fda24880fd9bcaebf6f
Markbulg <mark.markbulg@yahoo.com>
2023-12-10 00:32:33 +0100

    2nd commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express   from 'express';
+import rtspRelay from 'rtsp-relay';
+
+const port = process.env.PORT || 8091;
+
+const app = express()
+,     { proxy, scriptUrl } = rtspRelay(app);
+
+console.log('scriptUrl: ', scriptUrl);
+
+app.ws('/api/stream/:camUrl', (ws, req) => {
+	const params = req.params.camUrl.replace('-', '/');
+	console.log('req.params: ', req.params);
+	
+    return proxy({ url: `rtsp://${params}`, verbose: false })(ws);
+	//return proxy({ url: `rtsp://190.46.20.34:554/1` })(ws);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
