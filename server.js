431a21e9abf217e577ad9ee2e3325a916f0f0114
Markbulg <mark.markbulg@yahoo.com>
2023-12-14 21:50:40 +0100

    port added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>
         <script src='${scriptUrl}'></script>
         <script>
             loadPlayer({
-                url: 'ws://' + location.host + '/api/stream',
+                url: 'ws://' + location.host + ':' + port + '/api/stream',
                 canvas: document.getElementById('canvas')
             });
         </script>
