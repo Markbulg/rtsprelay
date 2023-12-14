9cc94e1b7dcf32a3a5301a869777a2f9bbd8e20b
Markbulg <mark.markbulg@yahoo.com>
2023-12-14 20:28:04 +0100

    test changed source

--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const port = process.env.PORT || 3000;
 const app = express()
 ,     { proxy, scriptUrl } = rtspRelay(app);
 
-console.log('scriptUrl: ', scriptUrl);
-
-app.ws('/api/stream/:camUrl', (ws, req) => {
-	const params = req.params.camUrl.replace('-', '/');
-	console.log('req.params: ', req.params);
-	
-    return proxy({ url: `rtsp://${params}`, verbose: false })(ws);
-	//return proxy({ url: `rtsp://190.46.20.34:554/1` })(ws);
+const handler = proxy({
+  url: `rtsp://190.46.20.34:554/1`,
+  verbose: false,
 });
+
+// the endpoint our RTSP uses
+app.ws('/api/stream', handler);
+
+app.get('/', (req, res) =>
+    res.send(`
+        <canvas id='canvas'></canvas>
+
+        <script src='${scriptUrl}'></script>
+        <script>
+            loadPlayer({
+                url: 'ws://' + location.host + '/api/stream',
+                canvas: document.getElementById('canvas')
+            });
+        </script>
+    `),
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
